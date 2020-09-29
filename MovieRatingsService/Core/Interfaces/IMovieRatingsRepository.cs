dc9d8a827bb3a6f2b32c74e7ccd3e6c1f0db19c7
Nbruunj <54842770+Nbruunj@users.noreply.github.com>
2020-09-29 12:13:05 +0200

    d

--- MovieRatingsService/Core/Interfaces/IMovieRatingsRepository.cs
+++ MovieRatingsService/Core/Interfaces/IMovieRatingsRepository.cs
@@ -6,16 +6,27 @@ namespace MovieRatingsApplication.Core.Interfaces
     public interface IMovieRatingsRepository
     {
         IList<MovieRating> GetAllMovieRatings();
+
         int GetNumberOfReviewsFromReviewer(int reviewer);
+        
         double GetAverageRateFromReviewer(int reviewer);
+        
         int GetNumberOfRatesByReviewer(int reviewer, int rate);
+        
         Int GetNumberOfReviews(int movie);
+        
         double GetAverageRateOfMovie(int movie);
+        
         int GetNumberOfRates(int movie, int rate);
+        
         List<int> GetMoviesWithHighestNumberOfTopRates();
+        
         List<int> GetMostProductiveReviewers();
+        
         List<int> GetTopRatedMovies(int amount);
+        
         List<int> GetTopMoviesByReviewer(int reviewer);
+        
         List<int> GetReviewersByMovie(int movie);
     }
 }
--- MovieRatingsService/Core/Model/MovieRating.cs
+++ MovieRatingsService/Core/Model/MovieRating.cs
@@ -5,18 +5,20 @@ using System.Text;
 namespace MovieRatingsApplication.Core.Model
 {
     public class MovieRating
-    {
-        public int Reviewer { get; private set; }
-        public int Movie { get; private set; }
-        public int Grade { get; private set; }
-        public DateTime Date { get; private set; }
 
-      public MovieRating(int r, int m, int g, DateTime d)
-        {
-            Reviewer = r;
-            Movie = m;
-            Grade = g;
-            Date = d;
-        }
+
+    {
+    public int Reviewer { get; private set; }
+    public int Movie { get; private set; }
+    public int Grade { get; private set; }
+    public DateTime Date { get; private set; }
+
+    public MovieRating(int r, int m, int g, DateTime d)
+    {
+        Reviewer = r;
+        Movie = m;
+        Grade = g;
+        Date = d;
+    }
     }
 }
