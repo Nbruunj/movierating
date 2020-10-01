e1f213599a8f803ca766253622790b6fdc4ed578
The Joker <54842770+Nbruunj@users.noreply.github.com>
2020-10-01 10:20:53 +0200

    k

--- MovieRatingsService/Core/Model/MovieRating.cs
+++ MovieRatingsService/Core/Model/MovieRating.cs
@@ -5,20 +5,18 @@ using System.Text;
 namespace MovieRatingsApplication.Core.Model
 {
     public class MovieRating
-
-
     {
-    public int Reviewer { get; private set; }
-    public int Movie { get; private set; }
-    public int Grade { get; private set; }
-    public DateTime Date { get; private set; }
+        public int Reviewer { get; private set; }
+        public int Movie { get; private set; }
+        public int Grade { get; private set; }
+        public DateTime Date { get; private set; }
 
-    public MovieRating(int r, int m, int g, DateTime d)
-    {
-        Reviewer = r;
-        Movie = m;
-        Grade = g;
-        Date = d;
-    }
+      public MovieRating(int r, int m, int g, DateTime d)
+        {
+            Reviewer = r;
+            Movie = m;
+            Grade = g;
+            Date = d;
+        }
     }
 }
